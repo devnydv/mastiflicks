a935549e6f69b6dc9cfbf0a4061e1fab1be0f325
devnydv <80939011+devnydv@users.noreply.github.com>
2024-05-05 23:30:54 +0530

    Update api.py
    test check

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
 import json
 import requests
 from dotenv import load_dotenv
-import os
-load_dotenv()
-db = os.getenv("db")
+#import os
+#load_dotenv()
+#db = os.getenv("db")
 
-
+db = "https://filmyapp-e1005.firebaseio.com/mastiflix.json"
 response = requests.get(db)
 data = response.json()
 
