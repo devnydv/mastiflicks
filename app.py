56a7170e13b5c0322151ad22f74d73179bf9855a
devnydv <80939011+devnydv@users.noreply.github.com>
2024-05-05 22:59:54 +0530

    Update app.py
    change to make it mastiflicks

--- app.py
+++ app.py
@@ -1,22 +1,37 @@
-import json 
-from flask import Flask, render_template
-from file1 import filehandle
+import json
+from flask import Flask, render_template, request
+from api import filehandle
+import requests
+
 app = Flask(__name__)
+data =json.loads( filehandle())
+#print(data)
+data = data["data"]
+reverseddata = data
+reverseddata.reverse()
+#@app.route("/")
+#def index():
+    #return render_template("index.html" ,dbdata= data)
 
 @app.route("/")
-def hello_world():
+def fil():
     
-    data =json.loads( filehandle())
+    cate = request.args.get("category")
+    if cate == None:       
+        return render_template("index.html" ,dbdata = reverseddata, cate = cate)
+    else:
+        
+        movies = [item for item in data if item["cat"] == cate]
+        return render_template("index.html" , cate = cate, dbdata = movies )
+
+
+@app.route("/read")
+def read():
+    id = request.args.get("id")
     #print(data)
-    data = data["lol"]["data"]
-    
-    return render_template("index.html", res =data, name= "lol" )
-
-
-@app.route("/lol/<int:marks>")
-def lol(marks):
-    return render_template("lol.html",mark = marks)
-
+    #id = int(id)
+    post = [item for item in reverseddata if item["id"] == id]
+    return render_template("read.html", ids = id, dbdata= post)
 
 if __name__ == "__main__":
     app.run(debug=True)
