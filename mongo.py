77747270350d6af8eb967ca411ff8e6634941d61
devnydv <80939011+devnydv@users.noreply.github.com>
2024-07-15 12:02:13 +0530

    Update mongo.py
    removal of MongoStr for security

--- mongo.py
+++ mongo.py
@@ -5,7 +5,7 @@ import os
 #database config
 api = os.environ.get("mongo")
 #api = os.getenv('mongo")
-#url = "mongodb+srv://bittumail:12356789@cluster0.fqrswkj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 url = api
 client = MongoClient(url)
 db = client.mastiflicks
