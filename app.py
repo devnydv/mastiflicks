b66c5b59293c9282219615020a4823e7d4b9bed6
devnydv <80939011+devnydv@users.noreply.github.com>
2024-07-15 11:19:21 +0530

    Update app.py
    added mongo code

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import json
 import math
+import datetime
 from flask import Flask, render_template, request
 from api import filehandle
-
+from mongo import catedata, catsize, readone, oneid, update, submit
+from bson import json_util
 
 app = Flask(__name__)
 data =json.loads( filehandle())
@@ -12,54 +14,106 @@ reverseddata = data
 reverseddata.reverse()
 postlen = len(reverseddata)
 
-#@app.route("/")
-#def index():
-    #return render_template("index.html" ,dbdata= data)
 
-@app.route("/")
-def fil():
-    page = request.args.get("page", 0 ,type=int)
-    page = page +1
-    start = 30*page - 30
-    end = 30*page
-    tota_page = postlen  / 30
-    tota_page = math.ceil(tota_page)
-    print(tota_page)
-    cate = request.args.get("category")
-    if cate == None:
-        return render_template("index.html",dbdata = reverseddata[start: end], cate = cate, pages= tota_page)
-    else:
-        movies = [item for item in data if item["cat"] == cate]
-        return render_template("index.html" , cate = cate, dbdata = movies )
-
+@app.route("/", methods = ["GET", "POST"])
+def formongo():
+    if request.method == "GET":
+        start = 0
+        end = 12 #not riquired
+        
+        data = catedata(start, "all")
+        cate = request.args.get("category")
+        
+        
+        if cate == None:
+            tota_page = catsize("all")
+        
+            return render_template("index.html", dbdata = data, cate = cate, pages= tota_page)
+           
+        else:
+            tota_page = catsize(cate)
+            data = catedata(start, cate )
+            return render_template("index.html" , cate = cate, dbdata = data )
+        
+    elif request.method == "POST":
+        cate = request.args.get("category")
+        page = request.args.get("page", 0 ,type=int)
+        page = page +1
+        start = 12*page - 12
+        end = 12*page
+        
+         
+        tota_page =  catsize(cate)
+        print(cate)
+        tota_page = tota_page / 12
+        tota_page = math.ceil(tota_page)
+        if tota_page >= page:
+            
+            data = catedata(start, cate )
+            return render_template('rhtml.html', dbdata = data, cate= cate)
+        else:
+            return "No More news"
 
 @app.route("/read")
 def read():
     id = request.args.get("id")
-    #print(data)
-    #id = int(id)
-# send 9 items for the recomandation section of read route
+
+    # send 9 items for the recomandation section of read route
     otherpost = []
-    if int(id) > 9:
+    if int(id) > 11:
         newid = int(id) 
-        lowid= newid -9
-        for x in range(lowid, newid):
-            otherpost.append([item for item in reverseddata if item["id"] == str(x)])
-        
+        data = catedata(0, "all")
+        otherpost.append(data)
     else:
         otherpost = []
 
-    # post contains a single post that is the main post
-    post = [item for item in reverseddata if item["id"] == id]
+    post =readone(id)
     return render_template("read.html", ids = id, dbdata= post, alldata= otherpost)
 
-@app.route("/newdb")
+@app.route("/newdb", methods = ["GET", "POST"])
 def newdb():
-    return render_template("newdb.html")
+    if request.method == "GET":
+        lastid = oneid()
+        for x in lastid:
+            idlast = int(x["id"]) +1
+        
+        timer = datetime.datetime.now()
+        timedata = f'{timer.strftime("%A")}, {timer.day} {timer.strftime("%b")}, {timer.strftime("%Y")}'
+        return render_template("db.html", tm = timedata, lid= idlast)
+    
+    elif request.method == "POST":
+        reqst = request.args.get("reqst")
+
+        if reqst == "fetch":
+            print("fetch")
+            id = request.args.get("id") 
+            
+            post = readone(id)
+            post = json_util.dumps(post)
+            return post
+        
+        if reqst== "update":
+            data = request.get_json()  # Get JSON data from the request
+            id = data['data']['id']
+            
+            update(data["data"], id)
+            
+
+        if reqst == "create":
+            data = request.get_json()
+            submit(data["data"])
+            
+        return render_template("db.html")
 
 @app.route("/privacy")
 def privacy():
     return render_template("privacy.html")
 
+#just for the test purpose 
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+    # code for index.py
+    #from wsgi import app
