1266aaf6d46f9dd31f70c169e862a42ab5dd710b
devnydv <80939011+devnydv@users.noreply.github.com>
2024-07-15 11:15:19 +0530

    Create mongo.py
    get data from mongo db instead of firebase api

---
+++ mongo.py
@@ -0,0 +1,40 @@
+import json
+from pymongo.mongo_client import MongoClient
+
+
+#database config
+url = "mongodb+srv://bittumail:12356789@cluster0.fqrswkj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(url)
+db = client.mastiflicks
+collection = db.data
+def oneid ():
+    return collection.find().sort('_id', -1).skip(0).limit(1)
+
+
+def catedata(start, cate):
+    if cate =="all":
+        return list(collection.find().sort('_id', -1).skip(start).limit(12))
+    else:
+        data = list(collection.find({"cat": cate}).sort('_id', -1).skip(start).limit(12))
+        return data
+
+def catsize(cate):
+    if cate =="all":
+        return collection.count_documents({})
+    else:
+        #amnt= collection.find({"cat": cate}).count()
+        return collection.count_documents({"cat": cate})
+    
+def readone(id):
+    onep = collection.find_one({'id':id})
+    return onep
+
+def update(updata, id):
+    collection.delete_one({"id": id})
+    collection.insert_one(updata)
+    
+
+
+def submit(subdata):
+    collection.insert_one(subdata)
+    print(subdata)
